100f9c6e04ff9018d91fb0cbbdf94e838c0a571d
s1veme <kpdpav@gmail.com>
2022-09-20 09:35:22 +0300

    update(cmd/app): create migrations on startup

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -5,7 +5,9 @@ import (
 	"go.uber.org/zap"
 	"smart-door/app/internal/applications/initiator"
 	"smart-door/app/internal/config"
+	"smart-door/app/pkg/database"
 	"smart-door/app/pkg/logging"
+	"smart-door/app/pkg/migrations"
 )
 
 func main() {
@@ -17,7 +19,12 @@ func main() {
 	appLogger := logging.NewLogger(logger, "smart-door")
 	logger.Info("init config")
 	cfg := config.GetConfig()
-
+	migrateManager := migrations.NewManager(database.DatabaseParametersToDSN("postgres", cfg.PostgreSQL.Host,
+		cfg.PostgreSQL.Database, cfg.PostgreSQL.Username, cfg.PostgreSQL.Password, false))
+	err = migrateManager.Migrate()
+	if err != nil {
+		logger.Fatal("migrate: ", zap.Error(err))
+	}
 	a, err := initiator.NewApp(cfg, appLogger)
 	if err != nil {
 		appLogger.Fatal("Error create app", zap.Error(err))
