713bf6ac5ff6d6bb3e4a68d1982b7dc8d9537a0b
s1veme <kpdpav@gmail.com>
2023-04-12 15:26:28 +0300

    add(migrations): add roles enum for users

--- migrations/00000001_inital_users_table.up.sql
+++ migrations/00000001_inital_users_table.up.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+CREATE TYPE roles AS ENUM ('admin', 'superadmin', 'user');
+
 CREATE TABLE users (
        id SERIAL PRIMARY KEY,
        person_id TEXT NOT NULL,
@@ -7,7 +9,7 @@ CREATE TABLE users (
        first_name TEXT NOT NULL,
        patronymic TEXT,
        last_name TEXT NOT NULL,
-       role TEXT NOT NULL,
+       role roles NOT NULL,
        phone TEXT,
        password TEXT,
        position TEXT NOT NULL,
