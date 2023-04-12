514b37f6e95df04757749e52f9846c8cdb1010f0
s1veme <kpdpav@gmail.com>
2023-04-12 15:32:47 +0300

    update(pkg/logging): add otelzap wrapper

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -1,7 +1,39 @@
 package logging
 
-import "go.uber.org/zap"
+import (
+	"context"
 
-func NewLogger(logger *zap.Logger, serviceName string) *zap.Logger {
-	return logger.Named(serviceName)
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger interface {
+	Debug(msg string, fields ...zapcore.Field)
+	Info(msg string, fields ...zapcore.Field)
+	Warn(msg string, fields ...zapcore.Field)
+	Error(msg string, fields ...zapcore.Field)
+	Panic(msg string, fields ...zapcore.Field)
+	Fatal(msg string, fields ...zapcore.Field)
+
+	With(fields ...zapcore.Field) *zap.Logger
+	Named(s string) *zap.Logger
+	Check(lvl zapcore.Level, msg string) *zapcore.CheckedEntry
+	Sync() error
+	Ctx(ctx context.Context) otelzap.LoggerWithCtx
+	WithOptions(opts ...zap.Option) *otelzap.Logger
+	Sugar() *otelzap.SugaredLogger
+	Clone(opts ...otelzap.Option) *otelzap.Logger
+	DebugContext(ctx context.Context, msg string, fields ...zapcore.Field)
+	InfoContext(ctx context.Context, msg string, fields ...zapcore.Field)
+	WarnContext(ctx context.Context, msg string, fields ...zapcore.Field)
+	ErrorContext(ctx context.Context, msg string, fields ...zapcore.Field)
+
+	DPanicContext(ctx context.Context, msg string, fields ...zapcore.Field)
+	PanicContext(ctx context.Context, msg string, fields ...zapcore.Field)
+	FatalContext(ctx context.Context, msg string, fields ...zapcore.Field)
+}
+
+func NewLogger(logger *zap.Logger, serviceName string) *otelzap.Logger {
+	return otelzap.New(logger.Named(serviceName))
 }
