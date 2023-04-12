89391067f4fceffe9081f922ad9ca4d99383ecfd
s1veme <kpdpav@gmail.com>
2023-04-12 15:31:16 +0300

    add(pkg): add tracer

---
+++ pkg/tracer/manage.go
@@ -0,0 +1,50 @@
+package tracer
+
+import (
+	"fmt"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func NewJaegerExporter(url string) (tracesdk.SpanExporter, error) {
+	return jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+}
+
+func NewTraceProvider(exp tracesdk.SpanExporter, serviceName string) (*tracesdk.TracerProvider, error) {
+	r, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(serviceName),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return tracesdk.NewTracerProvider(
+		tracesdk.WithBatcher(exp),
+		tracesdk.WithResource(r),
+	), nil
+}
+
+func NewTracer(jaegerURL string, serviceName string) (trace.Tracer, error) {
+	exporter, err := NewJaegerExporter(jaegerURL)
+	if err != nil {
+		return nil, fmt.Errorf("initialize exporter: %w", err)
+	}
+
+	tp, err := NewTraceProvider(exporter, serviceName)
+	if err != nil {
+		return nil, fmt.Errorf("initialize provider: %w", err)
+	}
+
+	otel.SetTracerProvider(tp)
+
+	return tp.Tracer("main tracer"), nil
+}
